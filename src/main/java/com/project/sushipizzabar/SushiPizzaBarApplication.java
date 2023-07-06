2655a98f55822e081a2d2ebc9f0de428bc4c031f
Ilya Shablinsky <74829307+ishablinskij@users.noreply.github.com>
2023-07-06 23:15:04 +0300

    Add files via upload

---
+++ src/main/java/com/project/sushipizzabar/SushiPizzaBarApplication.java
@@ -0,0 +1,12 @@
+package com.project.sushipizzabar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushiPizzaBarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SushiPizzaBarApplication.class, args);
+    }
+}
---
+++ src/test/java/com/project/sushipizzabar/SushiPizzaBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sushipizzabar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushiPizzaBarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
